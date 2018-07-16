3384012207bf3aa4725fdd71b6cde73cb809a1ee
Adrian Siekierka <kontakt@asie.pl>
2018-07-16 10:54:28 +0200

    more correctness

--- src/main/java/org/terasology/kallisti/oc/PeripheralOCFilesystem.java
+++ src/main/java/org/terasology/kallisti/oc/PeripheralOCFilesystem.java
@@ -59,7 +59,7 @@ public class PeripheralOCFilesystem implements Peripheral {
             byte[] data = file.read(bytes);
             // OpenComputers' reading relies on returning nil if the file
             // has been read fully.
-            if (bytes > 0 && data.length == 0) {
+            if (data != null && data.length == 0) {
                 return null;
             } else {
                 return data;
